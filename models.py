c5c5357208071f3adadff91129bda81fb64b9180
wanburana <wanjirasak@gmail.com>
2025-05-18 21:21:10 +0700

    add indexing, unique and null constraints

--- models.py
+++ models.py
@@ -1,3 +1,4 @@
+from typing import Optional
 from sqlalchemy import String
 from sqlalchemy.orm import Mapped, mapped_column
 from db import Model
@@ -7,11 +8,11 @@ class Product(Model):
     __tablename__ = 'products'
 
     id: Mapped[int] = mapped_column(primary_key=True)
-    name: Mapped[str] = mapped_column(String(64))
-    manufacturer: Mapped[str] = mapped_column(String(64))
-    year: Mapped[int]
-    country: Mapped[str] = mapped_column(String(32))
-    cpu: Mapped[str] = mapped_column(String(32))
+    name: Mapped[str] = mapped_column(String(64), index=True, unique=True)
+    manufacturer: Mapped[str] = mapped_column(String(64), index=True)
+    year: Mapped[int] = mapped_column(index=True)
+    country: Mapped[Optional[str]] = mapped_column(String(32))
+    cpu: Mapped[Optional[str]] = mapped_column(String(32))
 
 
     def __repr__(self):
